c900da9e091a75b964a5693190e51d815123df3c
teamon9161 <teamon9161@163.com>
2025-07-16 21:15:03 +0800

    [release] bump version to 0.3.0

--- pybond/test.py
+++ pybond/test.py
@@ -1,47 +1,56 @@
-import numpy as np
 import datetime
+
+import numpy as np
 import polars as pl
-from pybond.pl import TfEvaluators, Bonds
+
 # import os
 # os.environ["POLARS_VERBOSE"] = "1"
-
 from pybond import TfEvaluator
+from pybond.pl import Bonds, TfEvaluators
+
 e = TfEvaluator("T2509", 250205, "2025-07-15", 100, 0.02, 0.018)
 e.net_basis_spread
 
 
 length = 1000000
-futures = ["T2509"] * (length-2) + ["T2412", "T2509"]
-bonds = ["240215"] * (length-1) + ["240018"]
-dates = [datetime.date(2025, 5, 11)] * (length-3) + [datetime.date(2025, 5, 15)] * 3
+futures = ["T2509"] * (length - 2) + ["T2412", "T2509"]
+bonds = ["240215"] * (length - 1) + ["240018"]
+dates = [datetime.date(2025, 5, 11)] * (length - 3) + [datetime.date(2025, 5, 15)] * 3
 future_prices = np.random.rand(length) + 102
 bond_ytms = np.random.rand(length) * 0.001 + 0.02
 
 
-df = pl.DataFrame({
-    "future": futures,
-    "bond": bonds,
-    "date": dates,
-    "future_price": future_prices,
-    "bond_ytm": bond_ytms
-})
+df = pl.DataFrame(
+    {
+        "future": futures,
+        "bond": bonds,
+        "date": dates,
+        "future_price": future_prices,
+        "bond_ytm": bond_ytms,
+    }
+)
 
 
-# df.select(
-#     cp=Bonds("bond").clean_price(ytm="bond_ytm"),
-#     dp=Bonds("bond").dirty_price(ytm="bond_ytm"),
-#     ai=Bonds("bond").accrued_interest(),
-#     dv=Bonds("bond").duration(ytm="bond_ytm")
-# )
+df.select(
+    cp=Bonds("bond").clean_price(ytm="bond_ytm"),
+    dp=Bonds("bond").dirty_price(ytm="bond_ytm"),
+    ai=Bonds("bond").accrued_interest(),
+    dv=Bonds("bond").duration(ytm="bond_ytm"),
+)
 
-# import time
-# start = time.perf_counter()
-# print(df.select(TfEvaluators(capital_rate=0.018).net_basis_spread.alias("nbs")))
+import time
+
+start = time.perf_counter()
+print(df.select(TfEvaluators(capital_rate=0.018).net_basis_spread.alias("nbs")))
 # res = []
-# evaluator = TfEvaluator("T2412", "", datetime.date(1970, 1, 1), np.nan, np.nan, 0.018, 0)
+# evaluator = TfEvaluator(
+#     "T2412", "", datetime.date(1970, 1, 1), np.nan, np.nan, 0.018, 0
+# )
 # for i in range(length):
 #     # evaluator = TfEvaluator(futures[i], bonds[i], dates[i], future_prices[i], bond_ytms[i], 0.018)
-#     evaluator = evaluator.update(future_prices[i], bond_ytms[i], dates[i], futures[i], bonds[i], 0.018)
+#     evaluator = evaluator.update(
+#         future_prices[i], bond_ytms[i], dates[i], futures[i], bonds[i], 0.018
+#     )
 #     res.append(evaluator.net_basis_spread)
 # print(np.array(res))
-# print(f"Time taken: {time.perf_counter() - start:.6f} seconds")
+print(f"Time taken: {time.perf_counter() - start:.6f} seconds")
